5fabd060dafc11eddeae6730dc441be218651e61
Summerian-Winter <son.of.mithris@gmail.com>
2019-06-12 18:24:03 +0100

    Add client folder for front end biz using create-react app, added Footer, Landing and Navbar, Added authentication pages (Login, Register), talking to the backend API using axios

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">
+                  Welcome to the furnace
+                  <div className="awesomeness">where awesomeness is forged</div>
+                </h1>
+                <p className="lead">
+                  {" "}
+                  Create an AweSmith profile/portfolio, share posts and get help
+                  from other AweSmiths
+                </p>
+                <hr />
+                <Link to="/register" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </Link>
+                <Link to="/login" className="btn btn-lg btn-light">
+                  Login
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Landing;
