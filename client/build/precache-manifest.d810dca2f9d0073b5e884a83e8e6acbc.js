f1263f655be59cd38b4f6fea61984f34daea2a01
Summerian-Winter <son.of.mithris@gmail.com>
2019-07-14 02:48:09 +0100

    trying to deploy

---
+++ client/build/precache-manifest.d810dca2f9d0073b5e884a83e8e6acbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99cf5c3e33e3f6a4723d6f4b8411c1f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19de9cbcdd0526ef7668",
+    "url": "/static/css/main.ccd61610.chunk.css"
+  },
+  {
+    "revision": "f91d343438edb2564d54",
+    "url": "/static/js/2.316c5b13.chunk.js"
+  },
+  {
+    "revision": "19de9cbcdd0526ef7668",
+    "url": "/static/js/main.ec30d715.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
